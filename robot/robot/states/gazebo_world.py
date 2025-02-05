c48d46e772ac73ed0bf2dbbe2f09a1d433b1ab3a
ragaslan <agaslanramazan274@gmail.com>
2025-02-05 23:57:03 +0300

    change robot first pose in gazebo world

--- robot/robot/states/gazebo_world.py
+++ robot/robot/states/gazebo_world.py
@@ -9,8 +9,8 @@ class GazeboWorldState(State):
     def execute(self, blackboard: Blackboard) -> str:
         print("Loading Gazebo world...")
         
-        gazeboWorldProcess = subprocess.Popen(["ros2", "launch", "turtlebot3_gazebo", "turtlebot3_house.launch.py"])
-        #gazeboWorldProcess = subprocess.Popen(["ros2", "launch", "turtlebot3_gazebo", "turtlebot3_world.launch.py"])
+        gazeboWorldProcess = subprocess.Popen(["ros2", "launch", "turtlebot3_gazebo", "turtlebot3_house.launch.py", "x_pose:=-2.0", "y_pose:=0.5"])
+        #gazeboWorldProcess = subprocess.Popen(["ros2", "launch", "turtlebot3_gazebo", "turtlebot3_house.launch.py"])
         time.sleep(8)
         blackboard.__setitem__("gazebo_world_pid",gazeboWorldProcess.pid)
         return "gazebo_started"
